7ce422f648edd7a0ca19728aa656b7d4e903ee68
Patrick Shao <shao.pat@gmail.com>
2017-04-10 03:58:41 -0700

    Initial work on ensuring data security per user

--- duit-dev/duit-core/js/app.js
+++ duit-dev/duit-core/js/app.js
@@ -28,6 +28,9 @@
     promise.catch(e=>console.log(e.message));
   });
 
+
+
+
   //add signup event
   btnSignUp.addEventListener('click', e=> {
     //Get email and pass
@@ -35,10 +38,15 @@
     const pass = txtPassword.value;
     const auth = firebase.auth();
     //Sign in
-    const promise = auth.createUserWithEmailAndPassword(email,pass);
-    promise.catch(e=>console.log(e.message));
+    const promise = auth.createUserWithEmailAndPassword(email,pass).then(function(){
+      // On account creation, adds to the real-time database the uid 
+      firebase.database().ref('users/' + auth.currentUser.uid).set({
+        uid: auth.currentUser.uid
+      });  
+    }, function (reason) {
+      console.log(reason);
+    }); 
  
-
   });
 
   //add signout
@@ -47,17 +55,50 @@
   })
 
 //add a real time listener
-    firebase.auth().onAuthStateChanged(firebaseUser=>{
-      if (firebaseUser){
-        console.log(firebaseUser);
+    firebase.auth().onAuthStateChanged(user=>{
+      if (user){
+        console.log(user);
         btnLogout.classList.remove('hide');
-        // create SESSION VARIABLE of firebase user id
+        
+
+        // TODO: Associate this special token within the 
+        // MySQL database
+
+        // TODO: Any time there are any changes to the MySQL
+        // database, check for token
+
+        // Note: Special token can be uid, as even if an attacker
+        // knows the UID, they cannot spoof the id token that 
+        // is generated on a correct auth, which means that 
+        // as long as the function is used in a promise, 
+        // MySQL calls would only be made if the correct auth
+        // token is validated by Firebase
+
+        validateCall(function(){console.log("yay!");});
+
       } else {
         console.log('not logged in');
         btnLogout.classList.add('hide');
-        // clear session variable
       }
     })
 
+  function validateCall(callback){
+    var userId = firebase.auth().currentUser.uid;
+       firebase.database().ref('/users/' + userId).once('value').then(
+        function(snapshot) {
+          var uid = snapshot.val().uid;
+          console.log(uid);
+
+          //if validates from server data
+          if(userId === uid){
+            callback();
+          }
+    });
+  }
+
+
 }());
 
+
+//setInterval(function(){ console.log(firebase.auth().currentUser.uid);
+// }, 5000);
