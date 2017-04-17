5d399f5e3034e9608ffc7ef34edf9fdf73387dc1
Patrick Shao <shao.pat@gmail.com>
2017-04-17 04:27:24 -0700

    Add php functions to be done and setup parameters that are inputs these functions

--- duit-dev/duit-core/auth.php
+++ duit-dev/duit-core/auth.php
@@ -11,26 +11,51 @@
     //Todo: Add checks on validated call
     $idToken = $_GET["idToken"];
     $uid = $_GET["uid"];
-    $fName = $_GET["fName"];
-    // $fName = $_GET["zName"];
+    $function_name = $_GET["function_name"];
 
+    //All properties of a du
+    $du_id = $_GET["du_id"];
+    $du_timestamp = $_GET["du_timestamp"];
+    $du_name = $_GET["du_name"];
+    $du_has_date = $_GET["du_has_date"];
+    $du_has_deadline = $_GET["du_has_deadline"];
+    $du_has_duration = $_GET["du_has_duration"];
+    $du_time_start = $_GET["du_time_start"];
+    $du_time_end = $_GET["du_time_end"];
+    $du_priority = $_GET["du_priority"];
+    $du_enforce_priority = $_GET["du_enforce_priority"];
+    $tag_priorities = $_GET["tag_priorities"];
+    $calc_priority = $_GET["calc_priority"];
+    $du_note = $_GET["du_note"];
+    $du_tags = $_GET["du_tags"];
+    $du_status = $_GET["du_status"];
+    $user_id = $uid;
     
-    if ($fName=="displayAsTable"){
-      displayAsTable($all); //Note is possible to call $fName(all) 
-    } elseif ($fName=="add") {
-      add();
+    if ($function_name=="displayAsTable"){
+      displayTable_wrap($all); //Note is possible to call $function_name(all) 
+    } elseif ($function_name=="add") {
+      addDu_wrap();
     }
   }
 
-  function add(){
-    global $idToken, $uid, $all;
+  function addUser_wrap(){
 
-    //temporary uid override
-//    $uid = "7KlhTHGlsjQhXbvJDiW8toS0gtG3";
-    // echo $uid;
+  }
+
+  function deleteUser_wrap(){
+
+  }
+
+  function addDu_wrap(){
+    global $idToken, $uid, $all;
+    global $du_id, $du_timestamp, $du_name, $du_has_date, $du_has_deadline, $du_has_duration,
+           $du_time_start, $du_time_end, $du_priority, $du_enforce_priority, $tag_priorities,
+           $calc_priority, $du_note, $du_tags, $du_status, $user_id;
 
     if (validateToken($idToken,$uid)) {
-      $parameters = array('du_name' => 'Take out the trash' . rand(), 'du_has_date' => 1, 'du_time_start' => '2017-03-30', 'user_id' => $uid);
+      // $parameters = array('du_name' => 'Take out the trash' . rand(), 'du_has_date' => 1, 'du_time_start' => '2017-03-30', 'user_id' => $uid);
+
+      $parameters = array('du_name' => $du_name . rand(), 'du_has_date' => $has_date, 'du_time_start' => $time_start, 'user_id' => $uid);
       $all = addDu($parameters);
      // displayAsTable($all);
      // $all = deleteDu(5);
@@ -42,12 +67,16 @@
     }
   }
 
-  //assumes id is passed through something like a class variable
-  function delete(){
+  function editDu_wrap(){
 
   }
 
-  function displayTable(){
+  //assumes id is passed through something like a class variable
+  function deleteDu_wrap(){
+
+  }
+
+  function displayTable_wrap(){
     global $all;
 
     displayAsTable($all);
