6d6a3376e5297d0636272896f3267978612a6dba
Patrick Shao <shao.pat@gmail.com>
2017-04-17 05:11:42 -0700

    Add initialization of du variables and check for null values

--- duit-dev/duit-core/auth.php
+++ duit-dev/duit-core/auth.php
@@ -8,27 +8,31 @@
 
   if(!empty($_GET)) {
 
-    //Todo: Add checks on validated call
-    $idToken = $_GET["idToken"];
-    $uid = $_GET["uid"];
-    $function_name = $_GET["function_name"];
+
+    $idToken = (isset($_GET["idToken"])) ? $_GET["idToken"] : "";
+    $uid = (isset($_GET["uid"])) ? $_GET["uid"] : "";
+    $function_name = (isset($_GET["function_name"])) ? $_GET["function_name"] : "";
+
 
     //All properties of a du
-    $du_id = $_GET["du_id"];
-    $du_timestamp = $_GET["du_timestamp"];
-    $du_name = $_GET["du_name"];
-    $du_has_date = $_GET["du_has_date"];
-    $du_has_deadline = $_GET["du_has_deadline"];
-    $du_has_duration = $_GET["du_has_duration"];
-    $du_time_start = $_GET["du_time_start"];
-    $du_time_end = $_GET["du_time_end"];
-    $du_priority = $_GET["du_priority"];
-    $du_enforce_priority = $_GET["du_enforce_priority"];
-    $tag_priorities = $_GET["tag_priorities"];
-    $calc_priority = $_GET["calc_priority"];
-    $du_note = $_GET["du_note"];
-    $du_tags = $_GET["du_tags"];
-    $du_status = $_GET["du_status"];
+    $du_id = (isset($_GET["du_id"])) ? $_GET["du_id"] : "";
+
+    $du_timestamp = (isset($_GET["du_timestamp"])) ? $_GET["du_timestamp"] : "";
+    $du_name = (isset($_GET["du_name"])) ? $_GET["du_name"] : ""; //may have to set into null in order for intended behavior to occur
+    // echo(isset($du_name));
+
+    $du_has_date = (isset($_GET["du_has_date"])) ? $_GET["du_has_date"] : "";
+    $du_has_deadline = (isset($_GET["du_has_deadline"])) ? $_GET["du_has_deadline"] : "";
+    $du_has_duration = (isset($_GET["du_has_duration"])) ? $_GET["du_has_duration"] : "";
+    $du_time_start = (isset($_GET["du_time_start"])) ? $_GET["du_time_start"] : "";
+    $du_time_end = (isset($_GET["du_time_end"])) ? $_GET["du_time_end"] : "";
+    $du_priority = (isset($_GET["du_priority"])) ? $_GET["du_priority"] : "";
+    $du_enforce_priority = (isset($_GET["du_enforce_priority"])) ? $_GET["du_enforce_priority"] : "";
+    $tag_priorities = (isset($_GET["tag_priorities"])) ? $_GET["tag_priorities"] : "";
+    $calc_priority = (isset($_GET["calc_priority"])) ? $_GET["calc_priority"] : "";
+    $du_note = (isset($_GET["du_note"])) ? $_GET["du_note"] : "";
+    $du_tags = (isset($_GET["du_tags"])) ? $_GET["du_tags"] : "";
+    $du_status = (isset($_GET["du_status"])) ? $_GET["du_status"] : "";
     $user_id = $uid;
     
     if ($function_name=="displayAsTable"){
@@ -55,7 +59,7 @@
     if (validateToken($idToken,$uid)) {
       // $parameters = array('du_name' => 'Take out the trash' . rand(), 'du_has_date' => 1, 'du_time_start' => '2017-03-30', 'user_id' => $uid);
 
-      $parameters = array('du_name' => $du_name . rand(), 'du_has_date' => $has_date, 'du_time_start' => $time_start, 'user_id' => $uid);
+      $parameters = array('du_name' => $du_name . rand(), 'user_id' => $uid);
       $all = addDu($parameters);
      // displayAsTable($all);
      // $all = deleteDu(5);
